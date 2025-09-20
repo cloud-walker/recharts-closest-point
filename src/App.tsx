ca6160c24a0d6be630b75ecf1e26d834eb1ade82
Luca Barone <baro.luc@gmail.com>
2025-09-20 16:03:25 +0200

    feat: update LineChart component to handle multiple quality metrics and improve styling
    - Refactored App component to generate data with two quality metrics (quality_1, quality_2).
    - Updated LineChart to render two Line components with different tones for quality metrics.
    - Enhanced Line component to accept a tone prop for dynamic stroke color based on a predefined color mapping.
    - Added TypeScript path aliases for better module resolution in tsconfig.
    - Integrated vite-tsconfig-paths plugin in Vite configuration for improved path handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { faker as F } from "@faker-js/faker";
-import * as C from "recharts";
+import {faker as F} from '@faker-js/faker'
+import * as C from 'recharts'
 
-import { css } from "../styled-system/css";
-import { Line } from "./LineChart";
+import {css} from '#/system/css'
+import {Line} from './LineChart'
 
 const data = F.helpers.multiple(
-  () => {
-    return {
-      id: F.string.uuid(),
-      amount: F.number.float({ min: 0, max: 1000, fractionDigits: 2 }),
-      date: F.date.past(),
-    };
-  },
-  { count: 30 }
-);
+	() => {
+		return {
+			id: F.string.uuid(),
+			quality_1: F.number.float({min: 0, max: 1}),
+			quality_2: F.number.float({min: 0, max: 1}),
+			date: F.date.past(),
+		}
+	},
+	{count: 30},
+)
 
 export function App() {
-  return (
-    <C.ResponsiveContainer className={css({ height: "svh!" })}>
-      <C.LineChart data={data}>
-        <Line type="monotone" dataKey="amount" stroke="#8884d8" />
-        <C.XAxis
+	return (
+		<C.ResponsiveContainer className={css({height: 'svh!'})}>
+			<C.LineChart data={data}>
+				<Line dataKey="quality_1" tone="blue" />
+				<Line dataKey="quality_2" tone="green" />
+				{/* <C.XAxis
           dataKey="date"
           tickFormatter={(date) => new Date(date).toLocaleDateString()}
         />
-        <C.YAxis />
-        <C.Tooltip
+        <C.YAxis /> */}
+				{/* <C.Tooltip
           labelFormatter={(label) => new Date(label).toLocaleDateString()}
-        />
-        {/* <C.CartesianGrid stroke="#ccc" /> */}
-      </C.LineChart>
-    </C.ResponsiveContainer>
-  );
+        /> */}
+				{/* <C.CartesianGrid stroke="#ccc" /> */}
+			</C.LineChart>
+		</C.ResponsiveContainer>
+	)
 }
--- src/LineChart/Line.tsx
+++ src/LineChart/Line.tsx
@@ -1,5 +1,32 @@
-import * as C from "recharts";
+import * as C from 'recharts'
 
-export function Line(props: C.LineProps) {
-  return <C.Line {...props} />;
+import {type ColorToken, token} from '#/system/tokens'
+
+type ExtractToneFromColorToken<T extends string> =
+	T extends `${infer Tone}.${string}` ? Tone : never
+
+const toneToStroke = {
+	blue: token('colors.blue.800'),
+	rose: token('colors.rose.800'),
+	indigo: token('colors.indigo.800'),
+	green: token('colors.green.800'),
+	yellow: token('colors.yellow.800'),
+	red: token('colors.red.800'),
+	purple: token('colors.purple.800'),
+	pink: token('colors.pink.800'),
+	gray: token('colors.gray.800'),
+} as const satisfies Partial<
+	Record<ExtractToneFromColorToken<ColorToken>, string>
+>
+
+export declare namespace Line {
+	interface Props extends Pick<C.LineProps, 'dataKey'> {
+		tone?: keyof typeof toneToStroke
+	}
+}
+
+export function Line({tone = 'gray', ...props}: Line.Props) {
+	return (
+		<C.Line isAnimationActive={false} {...props} stroke={toneToStroke[tone]} />
+	)
 }
